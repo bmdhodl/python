39a9edfc2ffb6b5a2cd53a243535998d561519a9
Pat <patrickhughes013@gmail.com>
2021-09-05 12:04:50 -0500

    Practice
    if, for, while

---
+++ Looping.py
@@ -0,0 +1,13 @@
+def ten4():
+    for x in range(1, 11):
+        print(x)
+
+ten4()
+
+def tenW(y):
+    print("\nWhile:")
+    while y < 11:
+        print(y)
+        y+=1
+
+tenW(1)
---
+++ Spam.py
@@ -0,0 +1,9 @@
+#get number for Spam
+spam = int(input("Enter number for spam: "))
+
+if spam == 1:
+    print("Hello")
+elif spam == 2:
+    print("Howdy")
+else:
+    print("Grettings!")
